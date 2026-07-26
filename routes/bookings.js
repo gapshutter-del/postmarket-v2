d6c40ebf8e5a27a516f4879c1814c1d6bafba964
gapshutter-del <ramoupi33@gmail.com>
2026-07-27 01:12:15 +0200

    Update bookings.js

--- routes/bookings.js
+++ routes/bookings.js
@@ -112,12 +112,7 @@ router.get("/incoming", authenticate, async (req, res) => {
 // -----------------------------------------------------------------------------
 // Create booking
 // -----------------------------------------------------------------------------
-router.get("/", (req, res) => {
-    res.json({
-        success: true,
-        message: "Bookings router is mounted."
-    });
-});
+
 router.post("/", authenticate, async (req, res) => {
     try {
 
