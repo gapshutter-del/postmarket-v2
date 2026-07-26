edcf3321e064d26fe507116a8d868d6670febedc
gapshutter-del <ramoupi33@gmail.com>
2026-07-26 12:31:41 +0200

    Delete config/resend.js

--- config/resend.js
+++
@@ -1,9 +0,0 @@
-const { Resend } = require("resend");
-
-if (!process.env.RESEND_API_KEY) {
-  throw new Error("Missing RESEND_API_KEY");
-}
-
-const resend = new Resend(process.env.RESEND_API_KEY);
-
-module.exports = resend;
