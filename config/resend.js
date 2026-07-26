4fad80ff999f163d370ccfba86b87051c45ff456
gapshutter-del <ramoupi33@gmail.com>
2026-07-26 13:10:34 +0200

    Dbug Resend environment

--- config/resend.js
+++ config/resend.js
@@ -1,5 +1,13 @@
 const { Resend } = require("resend");
 
+console.log(
+  "RESEND_API_KEY loaded:",
+  !!process.env.RESEND_API_KEY,
+  process.env.RESEND_API_KEY
+    ? process.env.RESEND_API_KEY.substring(0, 5) + "..."
+    : "(missing)"
+);
+
 if (!process.env.RESEND_API_KEY) {
   throw new Error("Missing RESEND_API_KEY");
 }
