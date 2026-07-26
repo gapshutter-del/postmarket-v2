e00450fd480d2ecacac38d6952bb6a8ca1f8f6f6
gapshutter-del <ramoupi33@gmail.com>
2026-07-26 12:33:07 +0200

    Add Resend configuration

---
+++ config/resend.js
@@ -0,0 +1,9 @@
+const { Resend } = require("resend");
+
+if (!process.env.RESEND_API_KEY) {
+  throw new Error("Missing RESEND_API_KEY");
+}
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+module.exports = resend;
